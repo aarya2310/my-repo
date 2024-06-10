fafd0d92cf0014edd18f42412d3565d0f47ea8d7
Aarya <aaryapatel021206@gmail.com>
2024-06-10 10:52:22 +0530

    commit 2

---
+++ Assiss 2.py
@@ -0,0 +1,13 @@
+def add_book():
+    a=(input('enter title of book'))
+    b=(input('enter name of author'))
+    c=(input('enter type of genre'))
+    tuple_book = a,b,c
+def add_to_library():
+    
+    # d=(input("tuple_book"))
+    list = [tuple_book]
+    print(list)
+add_to_library()
+
+
